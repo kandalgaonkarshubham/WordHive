8f2683de8ae6f147cbc9b427638b72dc49867d1c
Shubham Kandalgaonkar <kandalgaonkarshubham@gmail.com>
2023-07-27 14:35:06 +0530

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -7,49 +7,49 @@ const About = () => {
 
   useEffect(() => {
 
-    let contentWayPoint;
+    // let contentWayPoint;
 
-    contentWayPoint = () => {
-      var i = 0;
-      $(".animate-box").waypoint(
-        function (direction) {
-          if (
-            direction === "down" &&
-            !$(this.element).hasClass("animated-fast")
-          ) {
-            i++;
+    // contentWayPoint = () => {
+    //   var i = 0;
+    //   $(".animate-box").waypoint(
+    //     function (direction) {
+    //       if (
+    //         direction === "down" &&
+    //         !$(this.element).hasClass("animated-fast")
+    //       ) {
+    //         i++;
 
-            $(this.element).addClass("item-animate");
-            setTimeout(function () {
-              $("body .animate-box.item-animate").each(function (k) {
-                var el = $(this);
-                setTimeout(
-                  function () {
-                    var effect = el.data("animate-effect");
-                    if (effect === "fadeIn") {
-                      el.addClass("fadeIn animated-fast");
-                    } else if (effect === "fadeInLeft") {
-                      el.addClass("fadeInLeft animated-fast");
-                    } else if (effect === "fadeInRight") {
-                      el.addClass("fadeInRight animated-fast");
-                    } else {
-                      el.addClass("fadeInUp animated-fast");
-                    }
+    //         $(this.element).addClass("item-animate");
+    //         setTimeout(function () {
+    //           $("body .animate-box.item-animate").each(function (k) {
+    //             var el = $(this);
+    //             setTimeout(
+    //               function () {
+    //                 var effect = el.data("animate-effect");
+    //                 if (effect === "fadeIn") {
+    //                   el.addClass("fadeIn animated-fast");
+    //                 } else if (effect === "fadeInLeft") {
+    //                   el.addClass("fadeInLeft animated-fast");
+    //                 } else if (effect === "fadeInRight") {
+    //                   el.addClass("fadeInRight animated-fast");
+    //                 } else {
+    //                   el.addClass("fadeInUp animated-fast");
+    //                 }
 
-                    el.removeClass("item-animate");
-                  },
-                  k * 100,
-                  "easeInOutExpo"
-                );
-              });
-            }, 50);
-          }
-        },
-        { offset: "85%" }
-      );
-    };
+    //                 el.removeClass("item-animate");
+    //               },
+    //               k * 100,
+    //               "easeInOutExpo"
+    //             );
+    //           });
+    //         }, 50);
+    //       }
+    //     },
+    //     { offset: "85%" }
+    //   );
+    // };
 
-    contentWayPoint();
+    // contentWayPoint();
     
   }, []);
 
