2d6e3a6ccb47067c0973910fec2996d99dcba8b7
Shubham Kandalgaonkar <kandalgaonkarshubham@gmail.com>
2023-07-27 15:01:19 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const viteEnv = {};
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith('VITE_')) {
+    viteEnv[`import.meta.env.${key}`] = process.env[key];
+  }
+});
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
@@ -13,6 +20,14 @@ export default defineConfig({
       "/activateAPI": "http://localhost:5000/",
     },
   },
+  alias: {
+    '@': require('path').resolve(__dirname, 'src'),
+  },
+  define: viteEnv,
   plugins: [react()],
 });
+
+
+
+
 
